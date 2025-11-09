8a9f71b6d06d5850aa2c95c7c2eea2bac4604dfb
abelbakk <abelbakk@gmail.com>
2025-11-09 15:40:07 +0100

    improve handling of fonts on output images, add more colours for bboxes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SZTE IMN104L-2025/26/1-IMN104L-2 Logo Detector
+
+Use `docker-compose up dev` or `docker-compose up prod`.
--- pages/detector.py
+++ pages/detector.py
@@ -1,18 +1,51 @@
 import streamlit as st
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from model.mock_model import MockModel
+
+BBOX_COLORS = [
+    "#E6194B",
+    "#3CB44B",
+    "#0082C8",
+    "#F58231",
+    "#911EB4"
+]
 
 @st.cache_resource
 def load_model():
     return MockModel()
 
-model = load_model()
+def draw_detection(draw, det, image_height, color):
+    x1, y1, x2, y2 = det["bbox"]
+    label = f"{det['class']} ({det['confidence']})"
+    
+    draw.rectangle([x1, y1, x2, y2], outline=color, width=3)
+
+    font_size = max(15, int(image_height * 0.02))
+    font = ImageFont.load_default(size=font_size)
+
+    text_bbox = draw.textbbox((x1, y1), label, font=font)
+    text_width = text_bbox[2] - text_bbox[0]
+    text_height = text_bbox[3] - text_bbox[1]
+
+    label_x1 = x1
+    label_y1 = y1 - text_height - 8 
+    label_x2 = x1 + text_width + 8
+    label_y2 = y1
+
+    if label_y1 < 0:
+        label_y1 = y1
+        label_y2 = y1 + text_height + 8
+
+    draw.rectangle([label_x1, label_y1, label_x2, label_y2], fill=color)
+    draw.text((label_x1 + 4, label_y1 + 4), label, fill="white", font=font)
 
 st.title("Brand Logo Detector")
 st.caption("SZTE IMN104L-2025/26/1-IMN104L-2")
 
 st.write("The brand logo detector tries to find logos from an image which can be uploaded below. To see a list of brands the application can recognise, visit the Supported Brands page from navigation.")
 st.write("")
+
+model = load_model()
 
 uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
@@ -23,11 +56,9 @@ if uploaded_file is not None:
         classes = []
         draw = ImageDraw.Draw(image)
         
-        for det in detections:
+        for i, det in enumerate(detections):
             classes.append(f"{det['class']} ({det['confidence']})")
-            x1, y1, x2, y2 = det["bbox"]
-            draw.rectangle([x1, y1, x2, y2], outline="red", width=3)
-            draw.text((x1, y1 - 10), f"{det['class']} ({det['confidence']})", fill="red", font_size=20) # TODO: font size probably has to be adaptive
+            draw_detection(draw, det, image.height, BBOX_COLORS[i % len(BBOX_COLORS)])
 
-        st.write(f"Found the following classes (class (confidence)): {", ".join(classes)}")
+        st.write(f"Found the following classes (with confidence): {", ".join(classes)}")
         st.image(image, caption="Output", width="content")
